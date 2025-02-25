561be486cdf75bcaa8708d9d97ff31b4fc3cd8b8
joserpfilho <msreynaldojose@gmail.com>
2025-02-25 13:26:07 -0300

    Remove authentication buttons from Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,10 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button>Sign In</Button>
-        </SignInButton>
-      </SignedOut>
-
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
+      
     </div>
   );
 }
