b730c767d4c0d83423ce1aaa581ae6136370a7e6
joserpfilho <msreynaldojose@gmail.com>
2025-02-25 13:25:26 -0300

    Add DesktopNavBar component and integrate it into NavBar; remove ModeToggle from Home

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import React from "react";
+import DesktopNavBar from "./DesktopNavBar";
 
 function NavBar() {
   return (
@@ -8,12 +9,13 @@ function NavBar() {
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
             <Link
-              href=""
+              href="/"
               className="text-xl font-bold text-primary font-mono tracking-wider"
             >
               Socially
             </Link>
           </div>
+          <DesktopNavBar />
         </div>
       </div>
     </nav>
