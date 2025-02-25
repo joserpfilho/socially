7fae024cc58f1632c2c55e7d2b51fe121859eaad
José <msreynaldojose@gmail.com>
2025-02-24 21:20:33 -0300

    Implement NavBar component with navigation link and styling

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 function NavBar() {
   return (
-    <div>
-      
-    </div>
-  )
+    <nav className="sticky top-0 w-full border-b bg-backgrond/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
+      <div className="max-w-7xl mx-auto px-4">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <Link
+              href=""
+              className="text-xl font-bold text-primary font-mono tracking-wider"
+            >
+              Socially
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default NavBar
+export default NavBar;
